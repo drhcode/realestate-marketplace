5269a628243587429eef25e97dacceb685117ec0
drhcode <doriancocka@gmail.com>
2023-02-16 14:38:31 +0100

    created single listing page

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,33 @@ import SignUp from './pages/SignUp'
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Explore />} />
-                    <Route path="/offers" element={<Offers />} />
-                    <Route
-                        path="/category/:categoryName"
-                        element={<Category />}
-                    />
-                    <Route path="/profile" element={<PrivateRoute />}>
-                        <Route path="/profile" element={<Profile />} />
-                    </Route>
-                    <Route path="/sign-in" element={<SignIn />} />
-                    <Route path="/sign-up" element={<SignUp />} />
-                    <Route path="/create-listing" element={<CreateListing />} />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPassword />}
-                    />
-                </Routes>
-                <Navbar />
-            </Router>
-            <ToastContainer />
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App
